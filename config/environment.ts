f66f9d1677515036512aa282dd1e080cd77db10b
Santiago Rendón <52801967+Rendxn@users.noreply.github.com>
2021-12-11 22:44:20 -0500

    Feature/e2e ci (#9)
    * feat: added e2e yml workflow on atsea deploy
    
    * refactor: cleaned up workflow

--- config/environment.ts
+++ config/environment.ts
@@ -2,4 +2,4 @@ export default {
   SELENIUM_BASE_URL:
     process.env.SELENIUM_BASE_URL || 'http://host.docker.internal:8080',
   API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:8080',
-}
+} as const
